137831df079e68e6c7e83e89f4dc7ff38c9c5703
Nika Gorokhova <veronikaandreevnago@gmail.com>
2024-07-28 10:36:47 +0300

    Дипломная работа

---
+++ test_api_2_find_a_book_with_id.py
@@ -0,0 +1,11 @@
+import requests
+
+
+def test_api():
+    base_URL = 'https://web-gate.chitai-gorod.ru/api/v1'
+    headers = {
+    'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjIwNDg0MDEsImlhdCI6MTcyMTg4MDQwMSwiaXNzIjoiL2FwaS92MS9hdXRoL2Fub255bW91cyIsInN1YiI6Ijg3NTMxNzBjNjczZjk3MDBmMjRhMzM1ZjFkYmIwMThlOWYzMGZhMzQ3MDIxZjEwMDk4MWVjOGYxMTA5OGI5ZmUiLCJ0eXBlIjoxMH0.Kb2xUmmk1gEFUcWAW9QYHamqwun28w3OVQGErZFNamc'
+    
+    }
+    response = requests.get(base_URL + '/products/slug/master-i-margarita-3018590', headers=headers)
+    assert response.status_code == 200
